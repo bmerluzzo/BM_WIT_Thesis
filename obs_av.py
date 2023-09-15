93a1d2a399300aa883a2b0f95a06c1374d2febcd
bmerluzzo <136020437+bmerluzzo@users.noreply.github.com>
2023-09-15 15:34:33 -0400

    added logging and adjusted algorithm for testing

--- obs_av.py
+++ obs_av.py
@@ -35,18 +35,39 @@ Change the URI variable to your Crazyflie configuration.
 import logging
 import sys
 import time
-
+import sys
+import logging
+import time
+from threading import Event
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
+from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils.multiranger import Multiranger
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E8')
+position_estimate = [0, 0]
+deck_attached_event = Event()
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
+
+def param_deck_flow(_, value_str):
+    value = int(value_str)
+    print(value)
+    if value:
+        deck_attached_event.set()
+        print('Deck is attached!')
+    else:
+        print('Deck is NOT attached!')
+
+def log_pos_callback(timestamp, data, logconf):
+    print(data)
+    global position_estimate
+    position_estimate[0] = data['stateEstimate.x']
+    position_estimate[1] = data['stateEstimate.y']
 
 def is_close(range):
     MIN_DISTANCE = 0.3
@@ -60,7 +81,7 @@ def is_close(range):
 if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
-    x = 40
+    x = 60
     y = 0 
     fl = 0.05
     j = 0
@@ -68,58 +89,27 @@ if __name__ == '__main__':
     s = 0
 
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-        # We take off when the commander is created
+        scf.cf.param.add_update_callback(group='deck', name='bcFlow2', cb=param_deck_flow)
+
+        logconf = LogConfig(name='Position', period_in_ms=500)
+        logconf.add_variable('stateEstimate.x', 'float')
+        logconf.add_variable('stateEstimate.y', 'float')
+        scf.cf.log.add_config(logconf)
+        logconf.data_received_cb.add_callback(log_pos_callback)
+        
         with MotionCommander(scf) as mc:
             with Multiranger(scf) as mr:
                 time.sleep(2)
+                logconf.start()
                 for i in range(x):
                     mc.forward(fl)
                     if is_close(mr.front):
                         j = 0
+                        s = 0
                         mc.stop()
                         time.sleep(2)
 
                         if is_close(mr.right):
-                            while j == 0:
-                                if is_close(mr.front):
-                                    mc.left(fl)
-                                    y = y + 1
-                                else:
-                                    time.sleep(1)
-                                    mc.forward(fl*6)
-                                    i = i + 6
-                                    if is_close(mr.right):
-                                        mc.forward(fl)
-                                        i = i + 1
-                                    else:
-                                        time.sleep(1)
-                                        for k in range(y):
-                                            mc.right(fl)
-                                        y = 0
-                                        k = 0
-                                        j = 1
-
-                        elif is_close(mr.left):
-                            while j == 0:
-                                if is_close(mr.front):
-                                    mc.right(fl)
-                                    y = y + 1
-                                else:
-                                    time.sleep(1)
-                                    mc.forward(fl*6)
-                                    i = i + 6
-                                    if is_close(mr.right):
-                                        mc.forward(fl)
-                                        i = i + 1
-                                    else:
-                                        time.sleep(1)
-                                        for k in range(y):
-                                            mc.left(fl)
-                                        y = 0
-                                        k = 0
-                                        j = 1
-
-                        else:
                             while j == 0:
                                 if is_close(mr.front):
                                     mc.left(fl)
@@ -136,11 +126,58 @@ if __name__ == '__main__':
                                             time.sleep(1)
                                             for k in range(y):
                                                 mc.right(fl)
+                                            time.sleep(2)
                                             y = 0
                                             k = 0
                                             s = 1
                                             j = 1
-                            
+
+                        elif is_close(mr.left):
+                            while j == 0:
+                                if is_close(mr.front):
+                                    mc.right(fl)
+                                    y = y + 1
+                                else:
+                                    time.sleep(1)
+                                    mc.forward(fl*7)
+                                    i = i + 7
+                                    while s == 0:
+                                        if is_close(mr.left):
+                                            mc.forward(fl)
+                                            i = i + 1
+                                        else:
+                                            time.sleep(1)
+                                            for k in range(y):
+                                                mc.left(fl)
+                                            time.sleep(1)
+                                            y = 0
+                                            k = 0
+                                            s = 1
+                                            j = 1
+
+                        else:
+                            while j == 0:
+                                if is_close(mr.front):
+                                    mc.left(fl*2)
+                                    y = y + 2
+                                else:
+                                    time.sleep(1)
+                                    mc.forward(fl*6)
+                                    i = i + 6
+                                    while s == 0:
+                                        if is_close(mr.right):
+                                            mc.forward(fl)
+                                            i = i + 1
+                                        else:
+                                            time.sleep(1)
+                                            for k in range(y):
+                                                mc.right(fl)
+                                            time.sleep(2)
+                                            y = 0
+                                            k = 0
+                                            s = 1
+                                            j = 1
+                       
 
                         """elif is_close(mr.front) and is_close(mr.left) and is_close(mr.right):
                             while j == 0:
@@ -168,5 +205,5 @@ if __name__ == '__main__':
                                         y = 0
                                         k = 0
                                         j = 1"""
-
+                logconf.stop()    
                 time.sleep(2)
