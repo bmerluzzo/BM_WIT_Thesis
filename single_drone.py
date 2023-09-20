40aee3e0378312fca2952180586dd91295825037
bmerluzzo <136020437+bmerluzzo@users.noreply.github.com>
2023-09-20 14:45:23 -0400

    Added portion of obstacle avoidance, fruther addtions needed

--- single_drone.py
+++ single_drone.py
@@ -21,14 +21,95 @@ fl = 0.1
 deck_attached_event = Event()
 
 def obs_avoid(mc, mr, fl):
+
+    time.sleep(3)
+
+    while ob != 2:
+        
+        i = 0
+        i = i + 0
+        x = 0
+        ob = 0
+        flag = 0
+
+        if is_close(mr.right) or is_close(mr.front):
+
+            move_left_ob(mc,mr, fl*2)
+
+            while flag != 1:
+                if is_close(mr.front):
+                    while is_close(mr.front):
+                        ob = move_left_ob(mc, mr, fl)
+                        x = x + 1
+                        if ob == 1:
+                            flag = 1
+                    time.sleep(2)
+
+                else:
+                    move_front_ob(mc, mr, fl*5)
+                    if is_close(mr.front):
+                        time.sleep(2)
+                        while is_close(mr.front):
+                            move_front_ob(mc, mr, fl)
+                            if ob == 1:
+                                flag = 1
+                        time.sleep(2)
+                    else: 
+                        for i in range(x):
+                            move_right_ob(mc, mr, fl)
+                        flag = 1
+            ob = 2
     return
+
 
 def move_forward(mc, mr, fl):
     mc.forward(fl)
     if is_close(mr.front):
+        mc.stop()
         obs_avoid(mc, mr, fl)
     elif is_close(mr.top):
         mc.stop()
+    else:
+        return
+
+def move_front_ob(mc, mr, fl):
+    mc.front(fl)
+    if is_close(mr.front):
+        mc.stop()
+        
+    elif is_close(mr.top):
+        mc.stop()
+    else:
+        return
+    
+def move_right_ob(mc, mr, fl):
+    mc.right(fl)
+    if is_close(mr.right):
+        mc.stop()
+        return 1
+    elif is_close(mr.top):
+        mc.stop()
+        return 2
+    else:
+        return
+    
+    
+def move_left_ob(mc, mr, fl):
+    mc.left(fl)
+    if is_close(mr.left):
+        mc.stop()
+        return 1
+    elif is_close(mr.top):
+        mc.stop()
+        return 2
+    else:
+        return
+    
+def move_back_ob(mc, mr, fl):
+    mc.back(fl)
+    if is_close(mr.top):
+        mc.stop()
+        return 2
     else:
         return
 
@@ -91,32 +172,35 @@ if __name__ == '__main__':
 
         scf.cf.param.add_update_callback(group='deck', name='bcFlow2', cb=param_deck_flow)
 
-        logconf = LogConfig(name='Position', period_in_ms=500)
+        logconf = LogConfig(name='Position', period_in_ms=500) #Callback setup for positional data
         logconf.add_variable('stateEstimate.x', 'float')
         logconf.add_variable('stateEstimate.y', 'float')
         scf.cf.log.add_config(logconf)
         logconf.data_received_cb.add_callback(log_pos_callback)
 
         
-        with MotionCommander(scf) as mc:
+        with MotionCommander(scf) as mc:    #Start take off of drone
             with Multiranger(scf) as mr:
+
                 time.sleep(2)
-                logconf.start()
-                size = len(spX)
-                rotc = 1
-                rotn = 0
+                logconf.start()             #start positional logging 
+              
+                size = len(spX)             #used for pathing for loop
+                rotc = 1                    #current front of drone direction 
+                rotn = 0                    # North(1), East(2), South(3), West(4), relative to initial direction of the front of the drone
                 j = 0
-                for i in range(size):
+
+                for p in range(size):
                     
-                    xp = spX[i]
-                    yp = spY[i]
-                    xn = spX[i+1]
-                    yn = spY[i+1]
+                    xp = spX[p]         #set current and next x and y values for the setpoints
+                    yp = spY[p]
+                    xn = spX[p+1]
+                    yn = spY[p+1]
 
                     yd, xd = 0, 0
                     ym, xm = 0, 0
 
-                    if xp == xn and yp < yn:
+                    if xp == xn and yp < yn:       
                         
                         rotn = 1
 
