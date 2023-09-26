e1c42cbdf5172839ceac94cb572c0a9c083e29b6
bmerluzzo <136020437+bmerluzzo@users.noreply.github.com>
2023-09-26 17:07:04 -0400

    Wrote Init and Test functions for driver

--- Thermal Sensor/mlx90640deck.c
+++ Thermal Sensor/mlx90640deck.c
@@ -10,19 +10,38 @@
 #include "system.h"
 #include "MLX90640_API.h"
 
+#define MLX90640_TASK_STACKSIZE    (2 * configMINIMAL_STACK_SIZE)
+#define MLX90640_TASK_PRI 3
+#define MLX90640_TASK_NAME "MLX90640"
+
 static bool isInit;
 void mlx90640Task(void* arg);
 
 // Deck driver init function
 static void mlx90640Init()
 {
+  if (isInit)
+    return;
+
+  DEBUG_PRINT("Initializing MLX90640...\n");
+  i2cdevInit(I2C1_DEV);
+
+  xTaskCreate(mlx90640Task, MLX90640_TASK_NAME, MLX90640_TASK_STACKSIZE, NULL, MLX90640_TASK_PRI, NULL);
+
+  isInit = true;
+  DEBUG_PRINT("MLX90640 initialization complete!\n");
 
 }
 
 //Deck Driver Test Function 
 static bool mlx90640Test()
 {
+  DEBUG_PRINT("MLX90640Deck test\n");
+  
+  if (!isInit)
+    return false;
 
+  return true;
 }
 
 //Deck driver task function (collecting data and calculating temperature)
