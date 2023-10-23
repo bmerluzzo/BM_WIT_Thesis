49c6b9e8d85b781aed0ef100a47059c755e1742e
bmerluzzo <136020437+bmerluzzo@users.noreply.github.com>
2023-10-23 13:09:25 -0400

    Chnages for testing of temperature logging with multiple logging blocks

--- temp_log_test.py
+++ temp_log_test.py
@@ -13,7 +13,7 @@ from cflib.utils.multiranger import Multiranger
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E9')
 
 def log_temp_callback(timestamp, data, logconf):
-    print("Temperature: ", data)
+    print(data)
     
     
 
@@ -22,11 +22,28 @@ if __name__ == '__main__':
 
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
 
-        logconf = LogConfig(name='Temp', period_in_ms=500) 
-        logconf.add_variable('MLX90640.To', 'float')
+        logconf = LogConfig(name='Temp1', period_in_ms=500) 
+        logconf.add_variable('MLX1.To1', 'float')
+        logconf.add_variable('MLX1.To2', 'float')
+        logconf.add_variable('MLX1.To3', 'float')
+        logconf.add_variable('MLX1.To4', 'float')
+        logconf.add_variable('MLX1.To5', 'float')
+        logconf.add_variable('MLX1.To6', 'float')
         scf.cf.log.add_config(logconf)
         logconf.data_received_cb.add_callback(log_temp_callback)
 
+        logconf2 = LogConfig(name='Temp2', period_in_ms=500) 
+        logconf2.add_variable('MLX2.To1', 'float')
+        logconf2.add_variable('MLX2.To2', 'float')
+        logconf2.add_variable('MLX2.To3', 'float')
+        logconf2.add_variable('MLX2.To4', 'float')
+        logconf2.add_variable('MLX2.To5', 'float')
+        logconf2.add_variable('MLX2.To6', 'float')
+        scf.cf.log.add_config(logconf2)
+        logconf2.data_received_cb.add_callback(log_temp_callback)
+
         logconf.start()
+        logconf2.start()
         time.sleep(20)
         logconf.stop()
+        logconf2.stop()
