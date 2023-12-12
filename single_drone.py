70b0c3cbe3f2f0eea201bf2fca119700bc0a8ba6
bmerluzzo <136020437+bmerluzzo@users.noreply.github.com>
2023-12-12 14:52:34 -0500

    Modified estimator reset method

--- single_drone.py
+++ single_drone.py
@@ -9,6 +9,7 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.utils import uri_helper
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils.multiranger import Multiranger
+from cflib.crazyflie.syncLogger import SyncLogger
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -1327,8 +1328,45 @@ def reset_estimator(scf):
     cf.param.set_value('kalman.resetEstimation', '1')
     time.sleep(0.1)
     cf.param.set_value('kalman.resetEstimation', '0')
+    print("Estimator Reset\n")
+    wait_for_position_estimator(cf)
 
+def wait_for_position_estimator(scf):
+        print("Waiting for Estimator to Stabilize\n")
+        log_config = LogConfig(name='Kalman Variance', period_in_ms=500)
+        log_config.add_variable('kalman.varPX', 'float')
+        log_config.add_variable('kalman.varPY', 'float')
+        log_config.add_variable('kalman.varPZ', 'float')
 
+        var_y_history = [1000] * 10
+        var_x_history = [1000] * 10
+        var_z_history = [1000] * 10
+
+        threshold = 0.001
+
+        with SyncLogger(scf, log_config) as logger:
+            for log_entry in logger:
+                data = log_entry[1]
+
+                var_x_history.append(data['kalman.varPX'])
+                var_x_history.pop(0)
+                var_y_history.append(data['kalman.varPY'])
+                var_y_history.pop(0)
+                var_z_history.append(data['kalman.varPZ'])
+                var_z_history.pop(0)
+
+                min_x = min(var_x_history)
+                max_x = max(var_x_history)
+                min_y = min(var_y_history)
+                max_y = max(var_y_history)
+                min_z = min(var_z_history)
+                max_z = max(var_z_history)
+
+                if (max_x - min_x) < threshold and (
+                        max_y - min_y) < threshold and (
+                        max_z - min_z) < threshold:
+                    break
+        print("Estimator Stabilized\n")
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
@@ -1456,8 +1494,6 @@ if __name__ == '__main__':
         global gn   
 
         logconf.start() 
-        print("Position Logging\n")
-        time.sleep(60)
 
         with MotionCommander(scf, default_height = 0.4) as mc:    
             with Multiranger(scf) as mr:
